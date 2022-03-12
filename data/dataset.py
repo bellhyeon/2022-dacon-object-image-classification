e02c0ffe8fd578949a46c2146e8abbc773ba4543
bellhyeon <bellhyeon@naver.com>
2022-03-12 11:23:30 +0900

    Fix train augmentation

--- data/dataset.py
+++ data/dataset.py
@@ -28,51 +28,14 @@ def _get_transforms(use_augmentation: bool, img_size: int):
                 Rotate(30, p=0.5),
                 RandomRotate90(p=0.5),
                 Resize(img_size, img_size),
-                # HorizontalFlip(p=0.5),
-                # VerticalFlip(p=0.5),
-                # Normalize(
-                #     # mean=[0.485, 0.456, 0.406],
-                #     # std=[0.229, 0.224, 0.225],
-                #     mean=[0.4914, 0.4822, 0.4465],
-                #     std=[0.2470, 0.2435, 0.2616]
-                # ),
-                A.HorizontalFlip(),
-                A.VerticalFlip(),
-                OneOf(
-                    [
-                        A.IAAAdditiveGaussianNoise(),
-                        A.GaussNoise(),
-                    ],
-                    p=0.2,
+                HorizontalFlip(p=0.5),
+                VerticalFlip(p=0.5),
+                Normalize(
+                    # mean=[0.485, 0.456, 0.406],
+                    # std=[0.229, 0.224, 0.225],
+                    mean=[0.4914, 0.4822, 0.4465],
+                    std=[0.2470, 0.2435, 0.2616]
                 ),
-                OneOf(
-                    [
-                        A.MotionBlur(blur_limit=3, p=0.2),
-                        A.MedianBlur(blur_limit=3, p=0.1),
-                        A.Blur(blur_limit=3, p=0.1),
-                    ],
-                    p=0.2,
-                ),
-                A.ShiftScaleRotate(rotate_limit=15),
-                OneOf(
-                    [
-                        A.OpticalDistortion(p=0.3),
-                        A.GridDistortion(p=0.1),
-                        A.IAAPiecewiseAffine(p=0.3),
-                    ],
-                    p=0.2,
-                ),
-                OneOf(
-                    [
-                        A.CLAHE(clip_limit=2),
-                        A.IAASharpen(),
-                        A.IAAEmboss(),
-                        A.RandomBrightnessContrast(),
-                    ],
-                    p=0.3,
-                ),
-                A.HueSaturationValue(p=0.3),
-                Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
                 ToTensorV2(),
             ]
         )
@@ -80,12 +43,6 @@ def _get_transforms(use_augmentation: bool, img_size: int):
     return Compose(
         [
             Resize(img_size, img_size),
-            # Normalize(
-            #     # mean=[0.485, 0.456, 0.406],
-            #     # std=[0.229, 0.224, 0.225],
-            #     mean=[0.4914, 0.4822, 0.4465],
-            #     std=[0.2470, 0.2435, 0.2616]
-            # ),
             Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
             ToTensorV2(),
         ]
